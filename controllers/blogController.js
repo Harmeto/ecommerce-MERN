1dcf689ca83e3b723a962a73b31f21813536464b
Harmeto <bad.thg@gmail.com>
2023-08-18 18:51:27 -0400

    feat blog route and blogcontroller like and dislike functionalities

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -10,16 +10,16 @@ const validateMongoDbId = require("../utils/validateMongodbId");
  * @param {Object} _ - Request
  * @param {Object} res - Response
  * @throws {Error} Arroja error si no se envian los datos necesarios
- * @returns {Promise<void>} 
+ * @returns {Promise<void>}
  */
-const createBlog = asyncHandler(async(_, res)=>{
+const createBlog = asyncHandler(async (_, res) => {
   try {
-    const newBlog = await Blog.create(_.body)
-    res.json(newBlog)
+    const newBlog = await Blog.create(_.body);
+    res.json(newBlog);
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-})
+});
 
 /**
  * Actualiza un Blog
@@ -28,17 +28,18 @@ const createBlog = asyncHandler(async(_, res)=>{
  * @param {Object} _ - Request
  * @param {Object} res - Response
  * @throws {Error} Arroja error si no se envian los datos necesarios
- * @returns {Promise<void>} 
+ * @returns {Promise<void>}
  */
-const updateBlog = asyncHandler(async(_, res)=>{
-  const {id} = _.params
+const updateBlog = asyncHandler(async (_, res) => {
+  const { id } = _.params;
   try {
-    const updateBlog = await Blog.findByIdAndUpdate(id, _.body, {new: true})
-    res.json(updateBlog)
+    validateMongoDbId(id);
+    const updateBlog = await Blog.findByIdAndUpdate(id, _.body, { new: true });
+    res.json(updateBlog);
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-})
+});
 
 /**
  * Visualiza un blog, añade 1 vista al blog visitado
@@ -46,37 +47,38 @@ const updateBlog = asyncHandler(async(_, res)=>{
  * @function
  * @param {Object} _ - Request
  * @param {Object} res - Response
- * @throws {Error} Arroja error si no se encuentra el blog 
- * @returns {Promise<void>} 
+ * @throws {Error} Arroja error si no se encuentra el blog
+ * @returns {Promise<void>}
  */
-const getBlog = asyncHandler(async(_, res)=>{
-  const {id} = _.params
+const getBlog = asyncHandler(async (_, res) => {
+  const { id } = _.params;
   try {
-    const blog = await Blog.findById(id)
-    await Blog.findByIdAndUpdate(id, {$inc: {numViews: 1}}, {new: true})
-    res.json(blog)
+    validateMongoDbId(id);
+    const blog = await Blog.findById(id);
+    await Blog.findByIdAndUpdate(id, { $inc: { numViews: 1 } }, { new: true });
+    res.json(blog);
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-})
+});
 
 /**
- * Retorna todos los blogs 
+ * Retorna todos los blogs
  * @async
  * @function
  * @param {Object} _ - Request
  * @param {Object} res - Response
  * @throws {Error} Arroja error si se encuentra ningun blog
- * @returns {Promise<void>} 
+ * @returns {Promise<void>}
  */
-const getAllBlog = asyncHandler(async(_, res)=>{
+const getAllBlog = asyncHandler(async (_, res) => {
   try {
-    const allBlog = await Blog.find()
-    res.json(allBlog)
+    const allBlog = await Blog.find();
+    res.json(allBlog);
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-})
+});
 
 /**
  * Elimina un blog por id
@@ -85,16 +87,122 @@ const getAllBlog = asyncHandler(async(_, res)=>{
  * @param {Object} _ - Request
  * @param {Object} res - Response
  * @throws {Error} Arroja error si no se envian los datos necesarios
- * @returns {Promise<void>} 
+ * @returns {Promise<void>}
  */
-const deleteBlog = asyncHandler(async(_, res)=>{
-  const {id} = _.params
+const deleteBlog = asyncHandler(async (_, res) => {
+  const { id } = _.params;
   try {
-    const deleteBlog = await Blog.findByIdAndDelete(id)
-    res.json(deleteBlog)
+    validateMongoDbId(id);
+    const deleteBlog = await Blog.findByIdAndDelete(id);
+    res.json(deleteBlog);
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-})
+});
 
-module.exports = {createBlog, updateBlog, getBlog, getAllBlog, deleteBlog}
+/**
+ * Like one Blog
+ * @async
+ * @function
+ * @param {Object} _ - Request
+ * @param {Object} res - Response
+ * @throws {Error} Arroja error si no se encuentra un blog o no se envia a travez de un usuario
+ * @returns {Promise<void>}
+ */
+const likeBlog = asyncHandler(async (_, res) => {
+  const { blogId } = _.body;
+  try {
+    validateMongoDbId(blogId);
+    const blog = await Blog.findById(blogId);
+    const loginUser = _?.user?._id;
+    const isLiked = blog?.isLiked;
+
+    const alreadyDisliked = blog?.disLikes?.find(
+      ((userId) => userId?.toString() === loginUser?.toString())
+    );
+    if (alreadyDisliked) {
+      const blog = await Blog.findByIdAndUpdate(
+        blogId,
+        { $pull: { disLikes: loginUser }, isDislike: false },
+        { new: true }
+      );
+      res.json(blog);
+    }
+
+    if (isLiked) {
+      const blog = await Blog.findByIdAndUpdate(
+        blogId,
+        { $pull: { likes: loginUser }, isLiked: false },
+        { new: true }
+      );
+      res.json(blog);
+    } else {
+      const blog = await Blog.findByIdAndUpdate(
+        blogId,
+        { $push: { likes: loginUser }, isLiked: true },
+        { new: true }
+      );
+      res.json(blog);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+/**
+ * Dislike one Blog
+ * @async
+ * @function
+ * @param {Object} _ - Request
+ * @param {Object} res - Response
+ * @throws {Error} Arroja error si no se encuentra un blog o no se envia a travez de un usuario
+ * @returns {Promise<void>}
+ */
+const disLikeBlog = asyncHandler(async (_, res) => {
+  const { blogId } = _.body;
+  try {
+    validateMongoDbId(blogId);
+    const blog = await Blog.findById(blogId);
+    const loginUser = _?.user?._id;
+    const isDisLiked = blog?.isDislike;
+
+    const alreadyLiked = blog?.likes?.find(
+      ((userId) => userId?.toString() === loginUser?.toString())
+    );
+    if (alreadyLiked) {
+      const blog = await Blog.findByIdAndUpdate(
+        blogId,
+        { $pull: { likes: loginUser }, isLike: false },
+        { new: true }
+      );
+      res.json(blog);
+    }
+
+    if (isDisLiked) {
+      const blog = await Blog.findByIdAndUpdate(
+        blogId,
+        { $pull: { disLikes: loginUser }, isDislike: false },
+        { new: true }
+      );
+      res.json(blog);
+    } else {
+      const blog = await Blog.findByIdAndUpdate(
+        blogId,
+        { $push: { disLikes: loginUser }, isDislike: true },
+        { new: true }
+      );
+      res.json(blog);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+module.exports = {
+  createBlog,
+  updateBlog,
+  getBlog,
+  getAllBlog,
+  deleteBlog,
+  likeBlog,
+  disLikeBlog
+};
--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -5,12 +5,13 @@ const express = require('express')
  */
 const router = express.Router()
 const {authMiddleware, isAdmin} = require('../middlewares/auth')
-const { createBlog, updateBlog, getBlog, getAllBlog, deleteBlog } = require('../controllers/blogController')
+const { createBlog, updateBlog, getBlog, getAllBlog, deleteBlog, likeBlog } = require('../controllers/blogController')
 
 router.get('/', getAllBlog)
 router.post('/', authMiddleware, isAdmin, createBlog)
-router.put('/:id', authMiddleware, isAdmin, updateBlog)
+router.put('/edit/:id', authMiddleware, isAdmin, updateBlog)
 router.get('/:id', getBlog)
 router.delete('/:id', authMiddleware, isAdmin, deleteBlog)
+router.put('/likes', authMiddleware, likeBlog)
 
 module.exports = router
